c864ac1ab6902db7c409f8c1715d86b3cff1de9d
nooberfsh <nooberfsh@gmail.com>
2021-08-19 15:09:27 +0800

    move meta to ast

--- src/code_gen/gen_ast.rs
+++ src/code_gen/gen_ast.rs
@@ -1,21 +1,72 @@
 use iterable::Iterable;
 
 use crate::ast;
-use crate::code_gen::gen_meta::gen_meta;
-use crate::util::indent;
+use crate::util::{indent, trim};
 
 use super::*;
 
+const META: &str = r#"
+use std::fmt;
+use std::cmp::Eq;
+use std::hash::{Hash, Hasher};
+use std::ops::Deref;
+use std::ops::DerefMut;
+
+use crate::span::Span;
+
+#[derive(Debug, Clone, Copy)]
+pub struct NodeId(pub(crate) usize);
+
+#[derive(Clone)]
+pub struct N<T> {
+    pub span: Span,
+    pub id: NodeId,
+    pub t: T,
+}
+
+impl<T: fmt::Debug> fmt::Debug for N<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.t.fmt(f)
+    }
+}
+
+impl<T> Deref for N<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.t
+    }
+}
+
+impl<T> DerefMut for N<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.t
+    }
+}
+
+impl<T: PartialEq> PartialEq for N<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.t == other.t
+    }
+}
+
+impl<T: Eq> Eq for N<T> {}
+
+impl<T: Hash> Hash for N<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.t.hash(state)
+    }
+}
+"#;
+
 impl<'ast> CodeGen<'ast> {
     pub fn gen_ast(&self) -> String {
-        let meta = gen_meta();
         let body = (&self.mir.rules)
             .lazy_map(|r| self.gen_rule(r))
             .join("\n\n");
         let leaf_nodes = (&self.mir.leaf_nodes)
             .lazy_map(|r| self.gen_leaf_node(r))
             .join("\n\n");
-        format!("{}\n{}\n\n{}", meta, body, leaf_nodes)
+        format!("{}\n{}\n\n{}", trim(META), body, leaf_nodes)
     }
 
     fn gen_leaf_node(&self, node: &Ident) -> String {

244e9db27ecf6d0383ea0723243c736ed1332bf2
nooberfsh <nooberfsh@gmail.com>
2021-09-02 11:53:06 +0800

    add missing std primary

--- src/util.rs
+++ src/util.rs
@@ -3,7 +3,7 @@ use iterable::Iterable;
 pub fn is_std_primary(input: &str) -> bool {
     match input {
         "bool" | "u8" | "u16" | "u32" | "u64" | "u128" | "i8" | "i16" | "i32" | "i64" | "i128"
-        | "char" | "str" | "!" => true,
+        | "f32" | "f64" | "usize" | "isize" | "char" | "str" | "!" => true,
         _ => false,
     }
 }
